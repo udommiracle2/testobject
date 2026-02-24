f753299fd43d40903b5323d8bc440b6c2b7c1a3c
udommiracle2 <udommiracle2@gmail.com>
2026-02-24 11:07:21 +0100

    first commit

--- src/src/Student.java
+++ src/src/Student.java
@@ -1,9 +1,9 @@
-public class Main{
+public class Student{
     String name;
     int age;
     String course;
 
-    public Main(String name, int age, String course) {
+    public Student(String name, int age, String course) {
         this.name = name;
         this.age = age;
         this.course = course;
@@ -15,8 +15,8 @@ public class Main{
     }
 
     public static void main(String[] args) {
-        Main myObj = new Main("Miracle", 40, "Robotics");
-        Main myObj2 = new Main("Udom", 100, "Quantum Physics");
+        Student myObj = new Student("Miracle", 40, "Robotics");
+        Student myObj2 = new Student("Udom", 100, "Quantum Physics");
 
         myObj.introduce();
         myObj2.introduce();
